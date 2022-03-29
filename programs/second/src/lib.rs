1e11d067af1e1666ff5969f4f8f25401ebfa9337
Golubev Matvei <golubev_m@stilsoft.ru>
2022-03-29 23:47:23 +0300

    Update test.
    Add expect field data.

--- programs/second/src/lib.rs
+++ programs/second/src/lib.rs
@@ -1,15 +1,41 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("3kDnzvRdJtEPV5zUHJSJRxqHXueW5UcJEdxbs4b5iKrN");
 
 #[program]
 pub mod second {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, data: u64) -> Result<()> {
+        let data_account = &mut ctx.accounts.data_account;
+        data_account.data = data;
+        msg!("it's my first solana program");
+        Ok(())
+    }
+
+    pub fn update(ctx: Context<Update>, data: u64) -> Result<()> {
+        let data_account = &mut ctx.accounts.data_account;
+        data_account.data = data;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 8)]
+    pub data_account: Account<'info, DataAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub data_account: Account<'info, DataAccount>,
+}
+
+#[account]
+pub struct DataAccount {
+    pub data: u64,
+}
--- tests/second.ts
+++ tests/second.ts
@@ -1,16 +1,39 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { expect } from "chai";
 import { Second } from "../target/types/second";
 
-describe("second", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+const { SystemProgram } = anchor.web3;
 
+describe("second", () => {
+  // Use a local provider.
+  const provider = anchor.Provider.local();
+
+  // Configure the client to use the local cluster.
+  anchor.setProvider(provider);
+
+  // The program to execute.
   const program = anchor.workspace.Second as Program<Second>;
+
+  // The Account to create.
+  const dataAccount = anchor.web3.Keypair.generate();
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.rpc.initialize({});
+    const tx = await program.methods.initialize(new anchor.BN(123))
+      .accounts({
+        dataAccount: dataAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([dataAccount])
+      .rpc();
+
+    let data = await program.account.dataAccount.fetch(dataAccount.publicKey);
+
+    expect(data.data.toNumber()).to.equal(123);
+
+
     console.log("Your transaction signature", tx);
   });
 });
